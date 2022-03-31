b0232db7ee14cb9b0d177133a71d6f320a04fb6d
noweder <hmnoweder@gmail.com>
2022-03-31 18:41:08 +0000

    updated dockerfile and app.py

--- dev1/app/app.1.py
+++ dev1/app/app.1.py
@@ -9,8 +9,8 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 @app.route('/', methods=['GET'])
 def hello_world():
-     return jsonify({"data": {"name" : os.getenv('HOSTNAME') ,"temp" : random.randrange(-10, 21, 1)}})
+     return jsonify({"data": {"name" : os.getenv('HOSTNAME') ,"temp" : random.randrange(-10, 20, 1)}})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1')
